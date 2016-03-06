eb81eb6d1d6bf0bb45582d6465d0d40cc948df82
U10416005 <r3r346@gmail.com>
2016-03-07 00:40:55 +0800

    Debug respond for RepeatAdditionQuiz
    Debug respond for using set and get methods in RepeatAdditionQuiz

--- TestRepeatAdditionQuiz.java
+++ TestRepeatAdditionQuiz.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 public class TestRepeatAdditionQuiz{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		RepeatAdditionQuiz og = new RepeatAdditionQuiz(72.0);
+		RepeatAdditionQuiz og = new RepeatAdditionQuiz(72,0);
 		System.out.println("What is 9 * 8? ");
-		og.setAns() = input.nextInt();
-		while(og.getAns() != og.geta()){
+		og.setAns(input.nextInt());
+		while(og.getAns() != og.getA()){
 			if(og.contains() == true){
 				System.out.println("You already entered " + og.getAns());
-				og.setAns() = input.nextInt();
+				og.setAns(input.nextInt());
 			}
 			else{
 				System.out.println("Wrong answer. Try again. What is 9*8? ");
+				og.setAns(input.nextInt());
 			}
 		}
 		System.out.println("You got it!");
