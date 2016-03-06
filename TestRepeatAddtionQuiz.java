8b5b38853203bc3b6e82452008bcc1f74c1d9dfb
U10416005 <r3r346@gmail.com>
2016-03-06 22:39:52 +0800

    Test class to run RepeatAdditionQuiz
    Write a test class which have scanner and invoking the RepeatAdditionQuiz  to run origin class

---
+++ TestRepeatAddtionQuiz.java
@@ -0,0 +1,9 @@
+//U10416005ªL«Ø¦t
+import java.util.Scanner;
+
+public class TestRepeatAdditionQuiz{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		RepeatAdditionQuiz og = new RepeatAddtionQuiz()
+	}
+}
