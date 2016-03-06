3cba576987f0adf83a4e23d2d2634e799bd09c62
U10416005 <r3r346@gmail.com>
2016-03-06 16:07:22 +0800

    Write class and constructor
    class and constructor

---
+++ RepeatAdditionQuiz.java
@@ -0,0 +1,14 @@
+//U10416005ªL«Ø¦t
+import java.util.ArrayList;
+
+public class RepeatAdditionQuiz{
+	private int a;
+	private int b;
+	private int ans;
+	public RepeatAdditionQuiz(int a,int b,int ans){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+}
