17fbeffdf656ca001ec735ba4a30af0ac35eacfd
U10416005 <r3r346@gmail.com>
2016-03-07 01:00:52 +0800

    Comment
    Write comment

--- TestRepeatAdditionQuiz.java
+++ TestRepeatAdditionQuiz.java
@@ -1,12 +1,17 @@
 //U10416005ªL«Ø¦t
+
+//import Scanner
 import java.util.Scanner;
 
 public class TestRepeatAdditionQuiz{
 	public static void main(String[] args){
+		//invoke Scanner
 		Scanner input = new Scanner(System.in);
+		//invoke subclass
 		RepeatAdditionQuiz og = new RepeatAdditionQuiz(72,0);
 		System.out.println("What is 9 * 8? ");
 		og.setAns(input.nextInt());
+		//use while including if to solve question
 		while(og.getAns() != og.getA()){
 			if(og.contains() == true){
 				System.out.println("You already entered " + og.getAns());
