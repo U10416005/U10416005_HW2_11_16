fb8541bd218cc360a693bf149357a7c06e7f9d06
U10416005 <r3r346@gmail.com>
2016-03-07 01:01:59 +0800

    Delete TestRepeatAddtionQuiz.java

--- TestRepeatAddtionQuiz.java
+++
@@ -1,9 +0,0 @@
-//U10416005ªL«Ø¦t
-import java.util.Scanner;
-
-public class TestRepeatAdditionQuiz{
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		RepeatAdditionQuiz og = new RepeatAddtionQuiz()
-	}
-}
