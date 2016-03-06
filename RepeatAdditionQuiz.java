23bc3cdee3e531f0c73bff810ace8aab5c270947
U10416005 <r3r346@gmail.com>
2016-03-06 22:38:20 +0800

    Delete one variable and import ArrayList
    Delete one extra variable b and import java.util.ArrayList

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -3,27 +3,20 @@ import java.util.ArrayList;
 
 public class RepeatAdditionQuiz{
 	private int a;
-	private int b;
 	private int ans;
-	public RepeatAdditionQuiz(int a,int b,int ans){
+	private ArrayList<int> list = new ArrayList();
+	public RepeatAdditionQuiz(int a,int ans){
 		this.a = a;
-		this.b = b;
 		this.c = c;
 	}
 	public int getA(int a){
 		return a;
-	}
-	public int getB(int b){
-		return b;
 	}
 	public int getAns(int ans){
 		return ans;
 	}
 	public void setA(){
 		this.a = a;
-	}
-	public void setB(){
-		this.b = b;
 	}
 	public void setAns(){
 		this.ans = ans;
